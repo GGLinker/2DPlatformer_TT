20f0dcdf3ce21d94e77573978e430973535844be
GGLinker <gglinker.channel@gmail.com>
2023-08-04 19:56:27 +0300

    CoinsCollect

--- 2DPlatformer Test Task/Assets/Scripts/Coins/CoinCollector.cs
+++ 2DPlatformer Test Task/Assets/Scripts/Coins/CoinCollector.cs
@@ -1,13 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class CoinCollector : MonoBehaviour
 {
+    [SerializeField] private TextMeshProUGUI coinsCountWidget;
     private int coinAmount = 0;
     
     public void CollectCoins(int amount)
     {
         coinAmount += amount;
+        Debug.Log(amount + " " + coinAmount);
+        coinsCountWidget.SetText(coinAmount.ToString());
     }
 }
--- 2DPlatformer Test Task/Assets/Scripts/Coins/CoinEntity.cs
+++ 2DPlatformer Test Task/Assets/Scripts/Coins/CoinEntity.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 [RequireComponent(typeof(Animator))]
 public class CoinEntity : MonoBehaviour
 {
-    [SerializeField] private readonly int coinWorth;
+    [SerializeField] private int coinWorth;
     [SerializeField] private string IsCollectedAnimatorParameterName;
     [SerializeField] private AnimationClip collectionAnimationClip;
 
     private Animator animator;
+    private bool collected;
 
     void Start()
     {
@@ -18,8 +19,9 @@ public class CoinEntity : MonoBehaviour
     private IEnumerator OnTriggerStay2D(Collider2D col)
     {
         var collector = col.transform.GetComponentInParent<CoinCollector>();
-        if (collector != null)
+        if (!collected && collector != null)
         {
+            collected = true;
             collector.CollectCoins(coinWorth);
             animator.SetBool(IsCollectedAnimatorParameterName, true);
             yield return new WaitForSeconds(collectionAnimationClip.length + 1f);
